ebca636f55a6fb15bb9f5d2ca67a725c818bc87e
Siwon-Kim <siwonkim1108@gmail.com>
2022-09-17 18:09:52 -0400

    Add Database in the Test File

--- test/login_test.py
+++ test/login_test.py
@@ -1,6 +1,38 @@
 import pytest
+import sqlite3
 from inCollege.main import *
 
+@pytest.fixture(scope='module')
+def setupDatabase():
+  print("-----INSERT TO A DB-----\n")
+  database = sqlite3.connect("inCollege.db")
+  databaseCursor = database.cursor()
+  # databaseCursor.execute('''CREATE TABLE users(
+  #                           id INTEGER PRIMARY KEY ASC, 
+  #                           username TEXT, 
+  #                           password TEXT)''')
+  sampleAccounts = [
+        (1, 'test1', 'aaaaaaa!A1'),
+        (2, 'test2', 'aaaaaaa!A1'),
+        (3, 'test3', 'aaaaaaa!A1'),
+        (4, 'test4', 'aaaaaaa!A1'),
+    ]                         
+  databaseCursor.executemany('''INSERT INTO users VALUES (?, ?, ?)''', sampleAccounts)
+  database.commit()
+  yield database
+  database.close()
+
+# Story: Account Creation
+
+# 1. username is unique
+def test_usernameUnique():
+  assert unique('test1') == False
+  assert unique('test2') == False
+  assert unique('test3') == False
+  assert unique('test4') == False
+  assert unique('test') == True
+
+# 2. password valiation
 @pytest.mark.parametrize('password, result',
                         [
                           ('abcdefgA!1', True),
@@ -11,5 +43,8 @@ from inCollege.main import *
                           ('abcdefgA1', False) # at least one special character
                         ]
                         )
-def test_passwordValidator(password, result):
+def test_passwordValidation(password, result):
   assert passwordValidator(password) == result
+
+# 3. username saved to a DB
+
