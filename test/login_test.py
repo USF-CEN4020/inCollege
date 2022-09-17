2c167c7a4261a8b0bbe92ee327dcae7f22d14af6
Siwon-Kim <siwonkim1108@gmail.com>
2022-09-17 14:26:51 -0400

    passwordValidator

---
+++ test/login_test.py
@@ -0,0 +1,15 @@
+import pytest
+from inCollege.main import *
+
+@pytest.mark.parametrize('password, result',
+                        [
+                          ('abcdefgA!1', True),
+                          ('abcdefg', False), # min 8 chars
+                          ('abcdefghijklmno', False), # max 12 chars
+                          ('abcdefgA!', False), # at least one digit
+                          ('abcdefg!1', False), # at least one capital letter
+                          ('abcdefgA1', False) # at least one special character
+                        ]
+                        )
+def test_passwordValidator(password, result):
+  assert passwordValidator(password) == result
