9a85e627f583536c69a63dffae74884214b3acea
kearschner <jacobkearschner@gmail.com>
2022-09-23 20:17:57 -0400

    readded testing functions

--- inCollege/main.py
+++ inCollege/main.py
@@ -33,6 +33,53 @@ database.commit()
 #----------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------
 
+# test purpose
+
+def loginStatus(username, password):
+  check = checkExistingAccts(username, password)
+  if check:
+    clear()
+    return True
+  else:
+    clear()
+    return False
+
+def stateMainInterface(username, password):
+  if loginStatus(username, password) == True:
+    return True
+  else:
+    return False
+
+def listOptions(sel):
+  if sel == '1' or sel == '2' or sel == '3':
+    return True
+  else:
+    return False
+
+def accountCount(count):
+  count = userCount()
+  if count > 5 or count < 0:
+    return False
+  else:
+    return True
+
+def listSkillsOptions(sel):
+  if sel == '1' or sel == '2' or sel == '3' or sel == '4' or sel == '5' or sel == '6':
+    return True
+  else:
+    return False
+
+def stateUnderConstruction(sel):
+  if sel == '1' or sel == '2' or sel == '4' or sel == '6' or sel == '8' or sel == '10':
+    return True
+  else: 
+    return False
+
+
+
+#----------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+
 
 
 def clearUsers():
