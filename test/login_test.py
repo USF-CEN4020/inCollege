7bcb4b05c8c5126fbd55d785a875a650e48e33d5
Siwon-Kim <siwonkim1108@gmail.com>
2022-09-25 13:15:47 -0400

    test successStory

--- test/login_test.py
+++ test/login_test.py
@@ -207,42 +207,42 @@ def test_underConstruction(sel, result):
 # Testing whether the sub-menu's that list additional options are returning the
 # the correct previous state if the specific option is selected
 
-  
+@pytest.mark.backtracking
 def test_mainInterfaceTrue():
   state = applicationEntry
   with mock.patch.object(builtins, 'input', lambda _: '4'):
     output, dataOut = mainInterface(-1)
     assert output == state
     
-    
+@pytest.mark.backtracking
 def test_mainInterfaceFalse():
   state = applicationEntry
   with mock.patch.object(builtins, 'input', lambda _: '1'):
     output, dataOut = mainInterface(-1)
     assert output != state
 
-
+@pytest.mark.backtracking
 def test_jobInterfaceTrue():
   state = mainInterface
   with mock.patch.object(builtins, 'input', lambda _: '3'):
     output, dataOut = jobInterface(-1)
     assert output == state
     
-    
+@pytest.mark.backtracking    
 def test_jobInterfaceFalse():
   state = mainInterface
   with mock.patch.object(builtins, 'input', lambda _: '2'):
     output, dataOut = jobInterface(-1)
     assert output != state
     
-    
+@pytest.mark.backtracking   
 def test_listSkillsTrue():
   state = mainInterface
   with mock.patch.object(builtins, 'input', lambda _: '6'):
     output, dataOut = listSkills(-1)
     assert output == state
     
-
+@pytest.mark.backtracking
 def test_listSkillsFalse():
   state = mainInterface
   with mock.patch.object(builtins, 'input', lambda _: '4'):
@@ -258,6 +258,7 @@ def test_listSkillsFalse():
 
 clearJobs()
   
+@pytest.mark.jobPosting
 @pytest.mark.parametrize('title, description, employer, location, salary, posterID, result',
                          [
                            ('CPE', 'stuff', 'Company4', 'Tampa, FL', '100000', '1', False),
@@ -267,10 +268,41 @@ clearJobs()
                            ('BIO', 'stuff', 'Company4', 'Tampa, FL', '100000', '5', True)
                          ]
 )
-
-
 def test_jobPost(title, description, employer, location, salary, posterID, result):
   inputs = iter([title, description, employer, location, salary, posterID])
   with mock.patch.object(builtins, 'input', lambda _: next(inputs)):
     state, data = jobPost(-1) 
     assert jobsFull() == result
+
+# testing whether the user can look up the existing first and last name
+
+clearUsers()
+
+@pytest.mark.userLookup
+@pytest.mark.parametrize('username, password, firstname, lastname, result',
+                        [
+                          ('test1', 'aaaaaaa!A1', 'first', 'last', 1),
+                          ('test2', 'aaaaaaa!A1', 'fname', 'lname', 2),
+                          ('test3', 'aaaaaaa!A1', 'f', 'l', 3),
+                          ('test4', 'aaaaaaa!A1', 'fff', 'lll', -1)
+                        ]
+                        )
+def test_userLookup(username, password, firstname, lastname, result):
+  inputs = iter([username, password, firstname, lastname])
+  with mock.patch.object(builtins, 'input', lambda _: next(inputs)):
+    state, data = newAcct()
+    assert checkExistingNames(firstname, lastname) == result
+
+# testing the extended account creation function
+# @pytest.mark.extendedAccountCreation
+
+
+# testing if the user can see a success story and have the option to play a related video
+@pytest.mark.successStory
+def test_successStoryState():
+  state = applicationEntry
+  with mock.patch.object(builtins, 'input', lambda _: '3'):
+    output, dataOut = videoPlayer()
+    assert output == state
+    
+      
