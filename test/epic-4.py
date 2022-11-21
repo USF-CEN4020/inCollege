2a9ab09b11adca708c83262ca029001031372f46
Marc Khan <mdkhan@usf.edu>
2022-11-20 19:57:39 -0500

    Fixed epic 4 test cases

--- test/epic-4.py
+++ test/epic-4.py
@@ -2,7 +2,7 @@ import pytest
 import builtins
 from unittest import mock
 from inCollege.manageDB import clearFriendships, clearUsers, friendshipsCount
-from inCollege.states import findFriends, findFriendsbyType, friendsList, mainInterface, newAcct, pendingRequest, requestFriends 
+from inCollege.states import findFriends, findFriendsbyType, friendsList, mainInterface, newAcct, handleFriendRequests, requestFriends 
 from inCollege.commons import *
 from inCollege.testFunc import *
 
@@ -25,10 +25,10 @@ from inCollege.testFunc import *
 
 def initTestAccounts():
 	accounts = [
-		('test1', 'aaaaaaa!A1', 'first', 'last', 'usf', 'cs'),
-        ('test2', 'aaaaaaa!A1', 'fname', 'lname', 'usf', 'ce'),
-        ('test3', 'aaaaaaa!A1', 'f', 'l', 'hcc', 'cs'),
-        ('test4', 'aaaaaaa!A1', 'fff', 'lll', 'NONE', 'NONE')
+		('test1', '123456A!', 'first', 'last', 'usf', 'cs', 'plus'),
+        ('test2', '123456A!', 'fname', 'lname', 'usf', 'ce', 'plus'),
+        ('test3', '123456A!', 'f', 'l', 'hcc', 'cs', 'plus'),
+        ('test4', '123456A!', 'fff', 'lll', 'NONE', 'NONE', 'plus')
 	]
 	for account in accounts:
 		inputs = iter(account)
@@ -57,38 +57,50 @@ def test_sendFriendRequestByLookup(senderId, recieverUsername, lookupSelection, 
 
 
 @pytest.mark.friends
-@pytest.mark.parametrize('response, numFriendsInSystem', [
-	('yes', 1),
-	('no', 0)
+@pytest.mark.parametrize('userId, response, numFriendsInSystem', [
+	(2, 'accept', 2),
+	(3, 'accept', 2)
 ])
-def test_friendRequest(response, numFriendsInSystem):
-	clearUsers()
-	clearFriendships()
-	initTestAccounts()
+def test_friendRequest(userId, response, numFriendsInSystem):
 
-	inputs = iter([' ', response, ' '])
+	inputs = iter([' ', ' ', response, ' '])
 
 	with mock.patch.object(builtins, 'input', lambda _: next(inputs)):
 		requestFriends(1, 'test2', 2) # hard-coded friend request from test1 to test2
-		pendingRequest(2)
+		requestFriends(1, 'test3', 3) # hard-coded friend request from test1 to test3
+		pendingRequests = checkExistingPendingRequest(userId) # get friend requests from database
+		handleFriendRequests(userId, pendingRequests)
 		assert friendshipsCount() == numFriendsInSystem
+  
+  
+# Story: Friends
+#		 As a developer, I want to add a friends list feature so that 
+# 		 user’s can connect with people they know.
+# Acceptance Criteria:
+# 		 User will have a “show my network” option that will present 
+#  		 the user with the list of people they’ve connected with (including none).
+@pytest.mark.friends
+@pytest.mark.parametrize('userId, friendId, result',
+                        [
+                        (1, 2, 1), #friend in the list
+                        (1, 1, -1),
+			  			(1, 0, -1)
+                        ]
+                        )
+def test_friendList(userId, friendId, result):
+    assert checkExistingFriend(userId, friendId) == result
 
 	
 @pytest.mark.friends
 @pytest.mark.parametrize('disconnectChoice, numFriendsInSystem', [
-	('test2', 0),
-	('0', 1)
+	('test2', 1),
+	('test3', 0)
 ])
 def test_disconnectFriend(disconnectChoice, numFriendsInSystem):
-	clearUsers()
-	clearFriendships()
-	initTestAccounts()
 
-	inputs = iter([' ', 'yes', ' ', disconnectChoice, ' '])
+	inputs = iter(['2', disconnectChoice, ' '])
 
 	with mock.patch.object(builtins, 'input', lambda _: next(inputs)):
-		requestFriends(1, 'test2', 2) # hard-coded friend request from test1 to test2
-		pendingRequest(2)
 
 		state, data = friendsList(1)
 		if state != mainInterface:
@@ -105,24 +117,3 @@ def test_disconnectFriend(disconnectChoice, numFriendsInSystem):
 @pytest.mark.accountNumber
 def test_accountCount():
   assert accountCount(0) == True # counting the number of users in actual DB 
-
-
-
-# Story: Friends
-#		 As a developer, I want to add a friends list feature so that 
-# 		 user’s can connect with people they know.
-# Acceptance Criteria:
-# 		 User will have a “show my network” option that will present 
-#  		 the user with the list of people they’ve connected with (including none).
-
-
-@pytest.mark.friends
-@pytest.mark.parametrize('userId, friendId, result',
-                        [
-                          (1, 2, 1), #friend in the list
-                          (1, 1, -1),
-			  (1, 0, -1),
-                        ]
-                        )
-def test_friendList(userId, friendId, result):
-    assert checkExistingFriend(userId, friendId) == result
